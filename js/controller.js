792acb4674b4cb63190649e68531c366aec28d79
iafellma <rinaldo.fellmann@stud.hslu.ch>
2018-05-19 13:40:58 +0200

    Suchfunktion eingebaut, Navigation ergänzt

--- js/controller.js
+++ js/controller.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     $('#toJazzLokalButton').click(searchLokal);
     //Ereignis: Jazz Event suchen
     $('#toJazzEventButton').click(searchEvent);
+     //Ereignis: Jazz Lokal 2 suchen
+    $('#toJazzLokal2Button').click(searchLokal2);
     //Ereignis: Standort anzeigen
     $('#showLocationMapButton').click(showMap);
     //Ereignis: Karte anzeigen
@@ -12,6 +14,258 @@ $(document).ready(function () {
 
     console.log("DOM ready");
 });
+
+
+function searchLokal2(){
+
+}
+
+
+/* Standort Funktionen */
+
+// Globale Var für Standort
+
+var currentLng;
+var currentLat;
+
+// Funktion für Standortbestimmung ohne speichern, die direkt mittels showMap angezeigt wird
+function showMapCurrentPosition(){
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showMap);
+    } else {
+        alert("Geolocation wird vom Browser nicht unterstuetzt");
+    }
+
+}
+
+// Karte anzeigen mit aktueller Position
+
+function showMap(position){
+
+    currentLng = position.coords.longitude;
+    sessionStorage.setItem("aktueller Längengrad", currentLng);
+    currentLat = position.coords.latitude;
+    sessionStorage.setItem("aktueller Breitengrad", currentLat);
+
+    var currentPosition = {
+        lat: currentLat,
+        lng: currentLng
+    };
+
+    //Optionen
+    var optionen = {
+        zoom: 13,
+        center: new google.maps.LatLng(currentLat, currentLng)
+    };
+
+    map = new google.maps.Map(document.getElementById('karteAusgabe'), optionen);
+
+    /*
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+    });*/
+
+    var image = 'https://cdn2.iconfinder.com/data/icons/IconsLandVistaMapMarkersIconsDemo/48/MapMarker_Marker_Outside_Chartreuse.png';
+    markerCurrent = new google.maps.Marker({
+        position: currentPosition,
+        map: map,
+        icon: image,
+
+        title: "Aktuelle Position"
+    });
+
+    for (var i=0; i<lokalLocations.length; i++){
+
+        var posAlleLokale = lokalLocations[i];
+
+        var locationLng = parseFloat(posAlleLokale.longitude);
+        var locationLat = parseFloat(posAlleLokale.latitude);
+
+        var positionAlleLokale = {
+            lat:locationLat,
+            lng:locationLng
+        }
+        //alert(locationLng+" "+locationLat);
+
+        var markerAlleLokale={
+            position: positionAlleLokale,
+            map: map,
+            title: posAlleLokale.bezeichnung
+        }
+
+
+        markerAlleLokale = new google.maps.Marker(markerAlleLokale);
+    }
+}
+
+
+
+//Funktion damit der Text bei der Namenssuche in den LocalStorage geschrieben wird
+function readName() {
+    var input = document.getElementById("nameLokal").value;
+
+    localStorage.setItem("textInput", input);
+}
+
+
+//Funktion damit bei den Select-Optionen des Umkreises der Wert ausgelesen und in den LocalStorage geschrieben wird
+function readUmkreis() {
+    var input = document.getElementById("umkreisLokal").value;
+
+    localStorage.setItem("umkreisInput", input);
+}
+
+
+
+//Funktion für die Resultatausgabe der Namenssuche
+function resultViewName() {
+
+    var resultListName = [];
+
+
+    var input = localStorage.getItem("textInput");
+
+    var titel;
+
+
+
+    for (i=0;i<4;i++){
+
+        var myLokal = lokalLocations[i];
+
+        var results = document.getElementById("resultateName");
+
+        resultListName[i] = myLokal;
+
+        //Berechnung der Entfernung in Kilometer anhand der Längen- und Breitengrade
+        var dx = 71.5 * (sessionStorage.getItem("aktueller Längengrad") - myLokal.longitude);
+        var dy = 111.3 * (sessionStorage.getItem("aktueller Breitengrad") - myLokal.latitude);
+        var distance = Math.sqrt(dx*dx + dy*dy);
+
+
+
+        if(myLokal.bezeichnung.toLowerCase().indexOf(input.toLowerCase())>=0) {
+
+
+            titel = document.createElement("a");
+            titel.setAttribute('href', 'http://localhost:63342/___FS2018_JazzApp/index.html?_ijt=71sfs612qctur523k3jb2bn7ki#page-lokal');
+
+
+            var adresse = document.createElement("div");
+            var leerschlag = document.createElement("br");
+            var linie = document.createElement("hr");
+            var distanz = document.createElement("div");
+
+
+            titel.className = "resultatTitel";
+            titel.innerText = resultListName[i].bezeichnung;
+            results.appendChild(titel);
+            adresse.className="resultatAdresse";
+            adresse.innerText = resultListName[i].adresse;
+            results.appendChild(adresse);
+            results.appendChild(leerschlag);
+
+            distanz.className = "resultatEntfernung";
+            distanz.innerText = "Entfernung: " +(Math.round(distance * 100) / 100) + " Kilometer";
+            results.appendChild(distanz);
+
+            results.appendChild(leerschlag);
+            results.appendChild(linie);
+
+        }
+
+
+    }
+    
+    //LocalStorage leeren damit bei erneutem Betätigen des Suchbuttons nicht nochmals alles ausgegeben wird
+    localStorage.clear();
+
+}
+
+
+
+
+//Funktion für die Resultatausgabe der Umnkreissuche
+function resultViewUmkreis() {
+
+    var resultListUmkreis = [];
+
+
+    var input = localStorage.getItem("umkreisInput");
+
+    var titel;
+
+
+
+    for (i=0;i<4;i++){
+
+        var myLokal = lokalLocations[i];
+
+        var results = document.getElementById("resultateUmkreis");
+
+        resultListUmkreis[i] = myLokal;
+
+        //Berechnung der Entfernung in Kilometer anhand der Längen- und Breitengrade
+        var dx = 71.5 * (sessionStorage.getItem("aktueller Längengrad") - myLokal.longitude);
+        var dy = 111.3 * (sessionStorage.getItem("aktueller Breitengrad") - myLokal.latitude);
+        var distance = Math.sqrt(dx*dx + dy*dy);
+
+        console.log(distance);
+
+        if(distance <= input){
+
+
+            titel = document.createElement("a");
+            titel.setAttribute('href', 'http://localhost:63342/___FS2018_JazzApp/index.html?_ijt=71sfs612qctur523k3jb2bn7ki#page-lokal');
+            var adresse = document.createElement("div");
+            var leerschlag = document.createElement("br");
+            var linie = document.createElement("hr");
+            var distanz = document.createElement("div");
+
+            titel.className = "resultatTitel";
+            titel.innerText = resultListUmkreis[i].bezeichnung;
+            results.appendChild(titel);
+            adresse.className="resultatAdresse";
+            adresse.innerText = resultListUmkreis[i].adresse;
+            results.appendChild(adresse);
+            results.appendChild(leerschlag);
+
+            distanz.className = "resultatEntfernung";
+            distanz.innerText = "Entfernung: " +(Math.round(distance * 100) / 100) + " Kilometer";
+            results.appendChild(distanz);
+
+
+            results.appendChild(leerschlag);
+            results.appendChild(linie);
+
+        }
+
+
+    }
+
+    //LocalStorage leeren damit bei erneutem Betätigen des Suchbuttons nicht nochmals alles ausgegeben wird
+    localStorage.clear();
+}
+
+
+//Funktionen damit bei betätigen des Zurück-Buttons und erneuter Suche, die vorherigen Resultate nicht mehr erscheinen
+function cleanName(){
+    document.getElementById("resultateName").innerHTML = "";
+
+}
+
+function cleanUmkreis() {
+    document.getElementById("resultateUmkreis").innerHTML = "";
+
+}
+
+
+
+
+
+
 
 function searchLokal() {
     if (navigator.geolocation) {
